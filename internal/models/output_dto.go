d8dbf37e25a93a26261c245ef8e82b1add53c126
DisVic <rogueone200534@gmail.com>
2024-10-26 12:46:41 +0300

    Revert "Merge pull request #1 from vet-clinic-back/develop"
    This reverts commit 68894cbc2860ad894490aec3a0e51ef29be6ac3e, reversing
    changes made to 99f55c5c5d16368e52fa416ebfdac6e8af844d0e.

--- internal/models/output_dto.go
+++
@@ -1,5 +0,0 @@
-package models
-
-type ErrorDTO struct {
-	Message string `json:"message"`
-}
--- internal/models/users.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Owner struct {
-	ID       uint   `json:"id"`
-	FullName string `json:"fullname"`
-	Email    string `json:"email,omitempty"`
-	Phone    string `json:"phone,omitempty"`
-	Password string `json:"password,omitempty"` // password hash
-}
